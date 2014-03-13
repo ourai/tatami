9745e2e6b5bd5df492f0513ad6d58713d28df721
Ourai Lin <ourairyu@hotmail.com>
2014-03-13 23:20:28 +0800

    U: settings of compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,32 @@
 module.exports = function( grunt ) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
-    coffee: {
+    dirs: {
+      src: "src",
+      dest: "dest/<%= pkg.version %>"
+    },
+    concat: {
       options: {
-        separator: "\n"
+        process: function( src, filepath ) {
+          return src.replace(/@VERSION/g, "<%= pkg.version %>");
+        }
       },
       build: {
-        expand: true,
-        cwd: "src/",
-        src: ["**/*.coffee"],
-        dest: "dest/",
-        ext: ".js"
+        src: ["<%= dirs.src %>/intro.coffee",
+              "<%= dirs.src %>/variable.coffee",
+              "<%= dirs.src %>/function.coffee",
+              "<%= dirs.src %>/core.coffee",
+              "<%= dirs.src %>/outro.coffee"],
+        dest: "<%= dirs.dest %>/<%= pkg.name %>.coffee"
+      }
+    },
+    coffee: {
+      options: {
+        separator: "\x20"
+      },
+      build: {
+        src: "<%= dirs.dest %>/<%= pkg.name %>.coffee",
+        dest: "<%= dirs.dest %>/<%= pkg.name %>.js"
       }
     },
     uglify: {
@@ -18,16 +34,19 @@ module.exports = function( grunt ) {
         banner: "/*! <%= pkg.name %> <%= grunt.template.today('yyyy-mm-dd') %> */\n"
       },
       build: {
-        src: "dest/<%= pkg.name %>.js",
-        dest: "dest/<%= pkg.name %>.min.js"
+        src: "<%= dirs.dest %>/<%= pkg.name %>.js",
+        dest: "<%= dirs.dest %>/<%= pkg.name %>.min.js"
       }
     }
   });
 
+  grunt.loadNpmTasks("grunt-contrib-concat");
   grunt.loadNpmTasks("grunt-contrib-coffee");
   grunt.loadNpmTasks("grunt-contrib-uglify");
 
-  grunt.registerTask("js2cs", "Compiles JavaScript files.", ["coffee"]);
+  grunt.registerTask("combine", "Concatenate CoffeeScript files.", ["concat"]);
+  grunt.registerTask("compile", "Compiles JavaScript files.", ["coffee"]);
+  grunt.registerTask("compress", "Uglify JavaScript files.", ["uglify"]);
 
-  grunt.registerTask("default", ["js2cs", "uglify"]);
+  grunt.registerTask("default", ["combine", "compile", "compress"]);
 };
